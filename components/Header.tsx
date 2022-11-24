42c7ff58e48e88560029094b7337ddf1645468d8
Tomohiro Takasaki <tomo.daidai@gmail.com>
2022-11-24 14:45:42 +0900

    [ update:dev ] commit test v9

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import styles from '../styles/Header.module.scss'
 const Header = () => {
   return (
     <header className={styles.container}>
-      <div>HEAD 8</div>
+      <div>HEAD 9</div>
     </header>
   )
 }
